24299f04c8def927dfa104476530810427d0cedf
adushyn <alena.dushyn@gmail.com>
2018-04-14 14:30:08 +0300

    String First task done

--- src/strings/practice/1.js
+++ src/strings/practice/1.js
@@ -6,13 +6,34 @@
 
 module.exports = function capFirstLowLast(str) {
 	'use strict';
-	// your code
+	let firstChar = str[0];
+	let firstCharUpper = firstChar.toUpperCase();
+
+	let lastChar = str[str.length - 1];
+	let lastCharLower = lastChar.toLowerCase();
+
+	let middleChars = str.slice(1, str.length - 1);
+
+	return firstCharUpper + middleChars + lastCharLower;
 
 };
 
 function capFirstLowLast(str) {
 	'use strict';
-};
+	let firstChar = str[0];
+	let firstCharUpper = firstChar.toUpperCase();
 
-console.log(capFirstLowLast('Ab3dD'));
+	let lastChar = str[str.length - 1];
+	let lastCharLower = lastChar.toLowerCase();
 
+	let middleChars = str.slice(1, str.length - 1);
+
+	return firstCharUpper + middleChars + lastCharLower;
+
+}
+
+console.log(capFirstLowLast('VovA'));
+console.log(capFirstLowLast('vova'));
+console.log(capFirstLowLast('VOVA'));
+console.log(capFirstLowLast('vOVA'));
+console.log(capFirstLowLast('VOVa'));
