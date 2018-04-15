c52d54d16e6e19a23de6eb1d1e6e9a3f6a03da1e
e211320 <oleksander.dushyn@evry.com>
2018-04-15 14:24:01 +0300

    task 4 and 5 for arrays

---
+++ src/arrays/practice/4.js
@@ -0,0 +1,19 @@
+/**
+ * Write function that
+ * take: array of numbers
+ * return: array with odd numbers only
+ * Example: oddNumbersForLoop([0,1,2,3,6]) -> [1, 3]
+ */
+function oddNumbersForLoop(arr){
+
+}
+
+/**
+ * Write function that
+ * take: array of numbers
+ * return: array with odd numbers only
+ * Example: oddNumbersForLoop([0,1,2,3,6]) -> [1, 3]
+ */
+function oddNumbersFilterLoop(arr){
+
+}
---
+++ src/arrays/practice/5.js
@@ -0,0 +1,19 @@
+/**
+ * Write function that
+ * take: array of numbers
+ * return: new array with square values from source array using `for` loop
+ * Example: arraySumWithForLoop([1,2,3]) -> [1, 4, 9]
+ */
+function sqrArrForLoop(arr){
+
+}
+
+/**
+ * Write function that
+ * take: array of numbers
+ * return: new array with square values from source array using `map` method
+ * Example: arraySumWithForLoop([1,2,3]) -> [1, 4, 9]
+ */
+function sqrArrMapLoop(arr){
+
+}
