6b29b70df850291c042dfeb78d9591bde2823572
e211320 <oleksander.dushyn@evry.com>
2018-04-15 09:59:03 +0300

    add task 2 and 3 for arrays

---
+++ src/arrays/practice/2.js
@@ -0,0 +1,13 @@
+/**
+ * Write function that prints array using "for" loop and console.log
+ */
+function loopWithFor(arr){
+
+}
+
+/**
+ * Write function that prints array using "forEach" loop and console.log
+ */
+function loopWithForEach(arr){
+
+}
---
+++ src/arrays/practice/3.js
@@ -0,0 +1,18 @@
+/**
+ * Write function that
+ * take: array of number
+ * return: sum of elements in array using for loop
+ * Example: arraySumWithForLoop([1,2,3]) -> 6
+ */
+function arraySumWithForLoop(arr){
+
+}
+
+/**
+ * Write function that
+ * take: array of number
+ * return: sum of elements in array using forEach loop
+ */
+function arraySumWithForEachLoop(arr){
+
+}
