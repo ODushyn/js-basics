e0171bc00f2a2b7f449d0f6dac1414bb7d874f53
adushyn <alena.dushyn@gmail.com>
2018-04-15 15:32:52 +0300

    exercise array 3

--- src/arrays/practice/3.js
+++ src/arrays/practice/3.js
@@ -4,15 +4,28 @@
  * return: sum of elements in array using for loop
  * Example: arraySumWithForLoop([1,2,3]) -> 6
  */
-function arraySumWithForLoop(arr){
-
+function arraySumWithForLoop(arr) {
+    let sum = 0;
+    for (let index = 0; index < arr.length; index++) {
+        sum = sum + arr[index];
+        //sum += arr[index];
+    }
+    return sum;
 }
+
+console.log(arraySumWithForLoop([1, 2, 3]));
 
 /**
  * Write function that
  * take: array of number
  * return: sum of elements in array using forEach loop
  */
-function arraySumWithForEachLoop(arr){
+function arraySumWithForEachLoop(arr) {
+    let sum = 0;
 
+    arr.forEach(function (element) {
+        sum = sum + element;
+    });
+    return sum;
 }
+console.log(arraySumWithForEachLoop([1, 2, 3]));
