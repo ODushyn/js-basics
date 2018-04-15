47dc54256e11a37ce161daa043cb42b922ded5f0
adushyn <alena.dushyn@gmail.com>
2018-04-15 16:51:14 +0300

    exercise array 4

--- src/arrays/practice/4.js
+++ src/arrays/practice/4.js
@@ -4,9 +4,19 @@
  * return: array with odd numbers only
  * Example: oddNumbersForLoop([0,1,2,3,6]) -> [1, 3]
  */
-function oddNumbersForLoop(arr){
 
+function oddNumbersForLoop(arr) {
+    var a = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            a.push(arr[i]);
+        }
+    }
+    return a;
 }
+
+console.log(oddNumbersForLoop([0, 1, 2, 3, 4, 5, 6]));
+
 
 /**
  * Write function that
@@ -14,6 +24,14 @@ function oddNumbersForLoop(arr){
  * return: array with odd numbers only
  * Example: oddNumbersForLoop([0,1,2,3,6]) -> [1, 3]
  */
-function oddNumbersFilterLoop(arr){
 
+function oddNumbersFilterLoop(arr) {
+    let newArray = arr.filter((elem, i) => {
+        if (elem % 2 !== 0) { 
+            return elem;
+         }
+    });
+    return newArray;
 }
+
+console.log(oddNumbersFilterLoop([0, 1, 2, 3, 4, 5, 6]));
